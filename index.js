a300beb938cf0661940e5e4977df5d0c56195bc2
unknown <mahadmirza681@gamail.com>
2022-12-16 23:27:12 +0500

    Excercise 12

--- index.js
+++ index.js
@@ -75,6 +75,10 @@ console.log(names[2]);
 // Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. 
 // The text of each message should be the same, but each message should be personalized with the person’s name.
 
+for(i = 0;i < names.length; i++){
+    console.log(`Hi there, ${names[i]}`);
+}
+
 // Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
 // Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
